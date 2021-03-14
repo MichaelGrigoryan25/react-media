5de4b2bc08507aba30eccff6ce5e1a2354f4cc01
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-03-14 12:25:04 +0400

    Major release

--- src/components/Asyncoload.jsx
+++ src/components/Asyncoload.jsx
@@ -1,19 +1,20 @@
 import axios from "axios";
 import React from "react";
-import { useEffect, useState, Fragment } from "react";
+import { useEffect, useState } from "react";
 
 const Media = ({ src, ...rest }) => {
   const [type, setType] = useState("");
   const [file, setFile] = useState({});
 
   useEffect(() => {
-    (async () => {
+    const fetchFile = async () => {
       if (src.length === 0) {
         throw new Error("No URL provided");
       } else {
         const { data } = await axios.get(src, {
           responseType: "blob",
         });
+
         setFile(
           URL.createObjectURL(
             new File([data], Date(), {
@@ -22,27 +23,22 @@ const Media = ({ src, ...rest }) => {
           )
         );
         setType(data.type);
-
-        return data;
       }
-    })();
-  }, [src]);
+    };
 
-  return (
-    <Fragment>
-      {type.startsWith("image") ? (
-        <img src={file} {...rest} alt={src} />
-      ) : type.startsWith("video") ? (
-        <video src={file} {...rest} alt={src}></video>
-      ) : type.startsWith("audio") ? (
-        <audio src={file} {...rest}></audio>
-      ) : (
-        <a href={src} target="_blank" rel="noreferrer" {...rest}>
-          See attached file
-        </a>
-      )}
-    </Fragment>
-  );
+    fetchFile();
+    return () => {};
+  }, []);
+
+  if (type.startsWith("image")) {
+    return <img src={file} {...rest} alt={src} />;
+  } else if (type.startsWith("video")) {
+    return <video src={file} {...rest} alt={src}></video>;
+  } else if (type.startsWith("audio")) {
+    return <audio src={file} {...rest}></audio>;
+  } else {
+    return null;
+  }
 };
 
 export default Media;
